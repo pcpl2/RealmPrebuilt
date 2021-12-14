2031dc38b2bad51ec524f6425a72aa7dacafa390
pcpl2 <pcpl2@users.noreply.github.com>
2021-12-14 17:25:23 +0000

    Updated realm version to v10.20.0

---
+++ Package.swift
@@ -0,0 +1,39 @@
+import PackageDescription
+
+let realmVersion = "10.20.0"
+
+func buildTargets() -> [Target] {
+    let baseURL = "https://github.com/pcpl2/RealmPrebuilt/releases/download/\(realmVersion)"
+    
+    let xcodeVersion = "13"
+    let realmChecksum = "9200517da6fb77eb9ba99b4158f64c2458aa21d403a2a6b84fcaebb80deb88f6"
+    let realmSwiftChecksum = "b1d4be6a3f08f322f066d59963bcfe23f7711122c8e7be674a0c371a2b4cbee7"
+    
+    return [
+        .binaryTarget(
+            name: "Realm",
+            url: "\(baseURL)/Realm_\(xcodeVersion).xcframework.zip",
+            checksum: realmChecksum
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "\(baseURL)/RealmSwift_\(xcodeVersion).xcframework.zip",
+            checksum: realmSwiftChecksum
+        )
+    ]
+}
+
+let package = Package(
+    name: "RealmPrebuilt",
+    platforms: [.iOS("13.4")],
+    products: [
+        .library(
+            name: "Realm",
+            targets: ["Realm"]),
+        .library(
+            name: "RealmSwift",
+            targets: ["RealmSwift"])
+    ],
+    targets: buildTargets()
+)
+
